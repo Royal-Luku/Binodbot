8ba60927fd27cbc4b69296d89c34cb1afd93103f
Royaldeep1 <120001510+Royaldeep1@users.noreply.github.com>
2023-06-12 07:46:25 +0530

    Update info.py

--- info.py
+++ info.py
@@ -37,7 +37,7 @@ DATABASE_NAME = environ.get('DATABASE_NAME', "Rajappan")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', 0))
+LOG_CHANNEL = int(environ.get('LOG_CHANNEL', "-1001972238867"))
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'Binod_Binod1)
 P_TTI_SHOW_OFF = is_enabled((environ.get('P_TTI_SHOW_OFF', "False")), False)
 IMDB = is_enabled((environ.get('IMDB', "True")), True)
